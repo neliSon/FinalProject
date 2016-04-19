160b5dc3d71793eeedcc4d222fe9e10a234da988
neliSun <nelson.chow91.nc@gmail.com>
2016-04-19 16:29:54 -0700

    installed realm

---
+++ FinalProject/Exercise.swift
@@ -0,0 +1,19 @@
+//
+//  Exercise.swift
+//  FinalProject
+//
+//  Created by Nelson Chow on 2016-04-19.
+//  Copyright © 2016 Nelson Chow. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Exercise: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
